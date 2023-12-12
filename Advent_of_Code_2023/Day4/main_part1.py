a44869f3d521911795648aee78988916b4f1ac44
danieltanner44 <153427303+danieltanner44@users.noreply.github.com>
2023-12-12 11:25:49 +0000

    update 2 day 4 part 1 solution

--- Advent_of_Code_2023/Day4/main_part1.py
+++ Advent_of_Code_2023/Day4/main_part1.py
@@ -1,8 +1,6 @@
 f = open(r'D:\Advent_of_Code\Advent_of_Code_2023\Day4\Puzzle_Input.txt', 'r')
 total_points = 0
 for each_line in f:
-    each_line = each_line.rstrip("\n").split()
-    if len(each_line) - len(list(set(each_line))) != 0:
-        total_points += 2**(len(each_line) - len(list(set(each_line)))-1)
-        print(total_points)
+    total_points += round(2**(len(each_line.rstrip("\n").split()) - len(list(set(each_line.rstrip("\n").split())))-1))
 f.close()
+print(total_points)
